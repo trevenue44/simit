f238a1729d96088722cb2a5c5426e0fd4e55f524
trevenue44 <trevenue44.main@gmail.com>
2023-07-06 17:13:31 +0000

    components pane docstrings

--- MainWindow/components_pane.py
+++ MainWindow/components_pane.py
@@ -11,6 +11,10 @@ from components.general import GeneralComponent
 
 class ComponentsPane(QtWidgets.QWidget):
     class Signals(QtCore.QObject):
+        """
+        A Qbject class to organise all signals that would be emitted from the ComponentsPane
+        """
+
         componentSelected = QtCore.pyqtSignal(wrappertype)
 
     def __init__(self, parent=None):
@@ -22,9 +26,12 @@ class ComponentsPane(QtWidgets.QWidget):
         self.initUI()
 
     def initUI(self):
+        # load QSS stylesheet and set that as the stylesheet of the ComponentsPane
         with open("./styles/components_pane.stylesheet.qss", "r") as f:
             styleSheet = f.read()
             self.setStyleSheet(styleSheet)
+
+        # making sure that the components pane is not any smaller than 250px
         self.setMinimumWidth(250)
 
         # vertical box layout to arrange everything vertically
@@ -44,10 +51,13 @@ class ComponentsPane(QtWidgets.QWidget):
         # creating a dropdown menu used to select component category
         self.componentCategory = QtWidgets.QComboBox()
         self.componentCategory.setPlaceholderText("Choose a component category")
+        # get a list of the category texts from the Component category enum that specifies the category a component belongs to
         categoryTexts = [category.value.capitalize() for category in ComponentCategory]
         categoryTexts.append("All")
+        # sorting the categories to make sure they're in alphabetical order
         categoryTexts.sort()
         self.componentCategory.addItems(categoryTexts)
+        # call the on component change function when the user changes the categry
         self.componentCategory.currentTextChanged.connect(
             self.onComponentCategoryChange
         )
@@ -58,12 +68,19 @@ class ComponentsPane(QtWidgets.QWidget):
             self.componentCategory, QtCore.Qt.AlignmentFlag.AlignTop
         )
 
+        # sets the initial state of the components category to "All" to display all compnents from the start
+        self.componentCategory.setCurrentText("All")
+
         # adding stretch to the bottom to push all the components up
         self.layout.addStretch()
         # using the vertical box layout as the layout of the component pane
         self.setLayout(self.layout)
 
     def clearComponents(self):
+        """
+        A utility function to clear every everything on the components pane except the first three i.e the searchbox, the line underneath it and the category combobox
+        It's used to clear the listed components
+        """
         while self.layout.count() > 3:
             item = self.layout.takeAt(3)
             widget = item.widget()
@@ -72,6 +89,9 @@ class ComponentsPane(QtWidgets.QWidget):
                 widget.deleteLater()
 
     def displayComponentClasses(self, componentClasses: list[Type["GeneralComponent"]]):
+        """
+        A function that takes in a list of the component classes to display and adds them to the layout
+        """
         for componentClass in componentClasses:
             label = QtWidgets.QLabel(componentClass.name)
             label.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
@@ -79,6 +99,12 @@ class ComponentsPane(QtWidgets.QWidget):
             self.layout.addWidget(label)
 
     def onSearchBoxTextChange(self, searchText: str):
+        """
+        A function that gets called whenever the text in the seach box changes.
+        It clears the current components in there and uses the searchText to get the filtered component classes.
+        It then calls the displayComponentClasses above to display the filtered component classes.
+        It adds a stretch to make sure the components are all pushed up
+        """
         self.clearComponents()
         componentClasses = getComponentClasses(searchText=searchText)
         # show components
@@ -87,6 +113,12 @@ class ComponentsPane(QtWidgets.QWidget):
         self.layout.addStretch()
 
     def onComponentCategoryChange(self, text):
+        """
+        A function that gets called whenever category in the comboxbox is changed by the user.
+        It clears the current components in there and uses the selected category to get the filtered component classes.
+        It then calls the displayComponentClasses above to display the filtered component classes.
+        It adds a stretch to make sure the components are all pushed up
+        """
         # clear the current components
         self.clearComponents()
         # extract selected category
@@ -102,6 +134,11 @@ class ComponentsPane(QtWidgets.QWidget):
         self.layout.addStretch()
 
     def onComponentClassClick(self, component: Type["GeneralComponent"]):
+        """
+        This function creates and returns the handler functions for each component that is displayed on components pane
+        It handler function is customized based on component to make sure that the right component calsses are emitted when clicked on
+        """
+
         def onComponentClick(event):
             self.signals.componentSelected.emit(component)
 
